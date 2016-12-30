9f774e42591a4576361d2183a7988986f1f63cfe
igoofyy <mgoofyy@gmail.com>
2016-12-30 10:16:34 +0800

    add touch event

--- src/StartGame/Layer/PlayGameLayer.js
+++ src/StartGame/Layer/PlayGameLayer.js
@@ -1,6 +1,7 @@
 var PlayGameLayer = cc.Layer.extend({
 
     _player : null,
+    _mapBg:null,
 
     ctor: function () {
         this._super();
@@ -10,6 +11,7 @@ var PlayGameLayer = cc.Layer.extend({
             x:GF.w_2,
             y:0
         });
+        this._mapBg = playGameLayer;
         this.addChild(playGameLayer);
 
         var player = new PlayerSprite(PLAYER.STATE.NORMAL);
@@ -19,7 +21,45 @@ var PlayGameLayer = cc.Layer.extend({
         });
         this.addChild(player);
         this._player = player;
+
+        this.touchInit();
+    },
+    
+    touchInit:function () {
+        if("touches" in cc.sys.capabilities){
+            cc.eventManager.addListener({
+                event: cc.EventListener.TOUCH_ONE_BY_ONE,
+                onTouchBegan: this._onTouchBegan.bind(this),
+                onTouchMoved: this._onTouchMove.bind(this)
+            }, this._mapBg);
+        } else {
+            console.log("-----")
+            cc.eventManager.addListener({
+                event: cc.EventListener.MOUSE,
+                swallowTouches:false,
+                onMouseDown: this._onMouseDown.bind(this)
+            }, this._mapBg);
+        }
+    },
+    _onTouchBegan: function (touch, event) {
+        console.log("click ")
+        return true;
     },
 
+    _onTouchMove: function(touch, event) {
+        var rect = touch.getLocation();
+        // console.log(rect.x,rect.y);
+        this._player.attr({
+            x:rect.x,
+            y:rect.y
+        });
+        return true;
+    },
+
+    _onMouseDown: function (touch,event) {
+
+
+
+    },
 
 });
